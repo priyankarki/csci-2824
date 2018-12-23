569fe1ce2f78c7fce0ce31394b5029fdb485a8c7
Priyanka Karki <40214689+priyankarki@users.noreply.github.com>
2018-12-23 00:00:32 -0700

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -2,17 +2,16 @@
 # element of the output list is a 0 if the number d is even, and 1 if d is odd, the second element of the output 
 # list is d/2 if d is even, and (d−1)/2 if d is odd.
 	
-	import array
-	def ParityParty(d):
-	    arr = []
-	    if d%2 == 0:
-	        arr.append(0)
-	        arr.append(int(d/2))
-	    else:
-	        arr.append(1)
-	        arr.append(int((d-1)/2))
-	    return arr
-
+import array
+def ParityParty(d):
+    arr = []
+    if d%2 == 0:
+	arr.append(0)
+	arr.append(int(d/2))
+    else:
+	arr.append(1)
+	arr.append(int((d-1)/2))
+    return arr
 
 
 # Write a Python function DecToBin() that takes in a nonnegative integer 𝚍 and returns a Python list of 0's and 1's 
