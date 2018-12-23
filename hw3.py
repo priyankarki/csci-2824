1dc4a0abc7e85cf9929b172bc9beea419595f2e3
Priyanka Karki <40214689+priyankarki@users.noreply.github.com>
2018-12-22 23:42:50 -0700

    Update hw3.py

--- hw3.py
+++ hw3.py
@@ -1,6 +1,6 @@
-Write a Python function D(i,n) that takes as input i and n, and returns a single logical value (True or False) 
-representing the truth value of D(i,n).
-	def D(i,n):
+# Write a Python function D(i,n) that takes as input i and n, and returns a single logical value (True or False) 
+# representing the truth value of D(i,n).
+def D(i,n):
     A = [4, 8, 15, 16, 23, 42]
     
     if(A[i] % n == 0):
@@ -8,9 +8,9 @@ representing the truth value of D(i,n).
     else:
         return False
 
-Write a Python function ABCs that takes in two lists of the same length, 𝚕𝚎𝚝𝚝𝚎𝚛𝚜 and 𝚗𝚞𝚖𝚋𝚎𝚛𝚜, and returns a single 
-logical value (True or False) representing the truth value of the following proposition.
-	def ABCs(letters, numbers):
+# Write a Python function ABCs that takes in two lists of the same length, 𝚕𝚎𝚝𝚝𝚎𝚛𝚜 and 𝚗𝚞𝚖𝚋𝚎𝚛𝚜, and returns a single 
+# logical value (True or False) representing the truth value of the following proposition.
+def ABCs(letters, numbers):
     out = False
     vowels = ['A','E','I',"O",'U','Y']
     for i in range(len(letters)):
@@ -19,10 +19,10 @@ logical value (True or False) representing the truth value of the following prop
                 out = True
     return out
 
-Write a function 𝚌𝚑𝚎𝚌𝚔_𝚙𝚛𝚘𝚙𝚘𝚜𝚒𝚝𝚒𝚘𝚗 that takes as its sole argument a Python list of between 0 and 50 integers and returns 
-a boolean (i.e. 𝚃𝚛𝚞𝚎 or 𝙵𝚊𝚕𝚜𝚎)  representing the truth value of the the proposition ∀x[E(x)→∃y(x=2y)], where here the 
-proposition E(x) means "x is even" and the list of integers is the domain for x and y.
-	def check_proposition(numbers):
+# Write a function 𝚌𝚑𝚎𝚌𝚔_𝚙𝚛𝚘𝚙𝚘𝚜𝚒𝚝𝚒𝚘𝚗 that takes as its sole argument a Python list of between 0 and 50 integers and returns 
+# a boolean (i.e. 𝚃𝚛𝚞𝚎 or 𝙵𝚊𝚕𝚜𝚎)  representing the truth value of the the proposition ∀x[E(x)→∃y(x=2y)], where here the 
+# proposition E(x) means "x is even" and the list of integers is the domain for x and y.
+def check_proposition(numbers):
     out = True
     for i in numbers:
         if (i % 2 == 0):
